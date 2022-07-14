70a85547bc4252f0ecf0af51328a31d2e2a035f3
Rahman Islam <54945042+RahmanIslamIen@users.noreply.github.com>
2022-07-15 01:49:58 +0700

    membuat category relationship pada post

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -2,7 +2,8 @@
 
 @section('container')
     @foreach ($posts as $post)
-        <article class="mb-5">
+        <h1 class="mb-5">Post Category : {{ $category }}</h1>
+        <article>
             <h2><a href="/posts/{{ $post->slug }}">{{ $post->title }}</a></h2>
             <h5>By. Rahman Islam in
                 <a href="/categories/{{ $post->category->slug }}">
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,7 +3,11 @@
 @section('container')
     <article>
         <h2><a href="/posts/{{ $post->id }}">{{ $post->title }}</a></h2>
-        <h5>By: {{ $post->author }}</h5>
+        <h5>By. Rahman Islam in
+            <a href="/categories/{{ $post->category->slug }}">
+                {{ $post->category->name }}
+            </a>
+        </h5>
         <p>{!! $post->body !!}</p>
     </article>
 
